e24eb115bc3db85ffcb3b76e61f708c08780b6ed
Ольга Захарова <kotfimn@mail.ru>
2026-04-05 16:31:06 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import vue from '@astrojs/vue'
 import { fileURLToPath } from 'node:url'
 import { URL } from 'node:url'
-import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
@@ -20,6 +20,5 @@ export default defineConfig({
         },
       },
     },
-  },
-  adapter: netlify(),
+  }
 })
