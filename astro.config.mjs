3129bfa5ea2bb5cab10802c8acfa9d785261b6d6
Ольга Захарова <kotfimn@mail.ru>
2026-04-05 16:37:42 +0700

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import vue from '@astrojs/vue'
 import { fileURLToPath } from 'node:url'
 import { URL } from 'node:url'
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
   output: 'server',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -20,5 +24,7 @@ export default defineConfig({
         },
       },
     },
-  }
+  },
+
+  adapter: vercel()
 })
